14265280f810dd636d2c538d2166ad81b51f15bf
laipp123 <laippmiles@gmail.com>
2018-08-29 19:29:29 +0800

    Code_JAVA

---
+++ java/HelloWorld/HelloWorldAgain.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HelloWorldAgain {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Scanner input = new Scanner(System.in);
+		String a = input.nextLine();
+		System.out.println("输入了："+a);
+	}
+
+}
---
+++ java/HelloWorld/helloworld.java
@@ -0,0 +1,11 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld again!");
+		System.out.println("ÈºÃæÊÇ¶ñÄ§£¡");
+	}
+
+}
